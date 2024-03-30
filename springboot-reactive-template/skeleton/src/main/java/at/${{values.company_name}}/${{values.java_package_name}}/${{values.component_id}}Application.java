c251ab0053b9a542cd18d9bd270614eda37a0256
avollmaier <alois.vollm@gmail.com>
2024-03-30 11:55:28 +0100

    Revert "record start time"
    This reverts commit e56506227b924072b097b81ab897dd27b0059eea.

--- springboot-reactive-template/skeleton/src/main/java/at/${{values.company_name}}/${{values.java_package_name}}/${{values.component_id}}Application.java
+++ springboot-reactive-template/skeleton/src/main/java/at/${{values.company_name}}/${{values.java_package_name}}/${{values.component_id}}Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ${{values.component_id}}Application {
+
   public static void main(String[] args) {
     SpringApplication.run(${{values.component_id}}Application.class, args);
   }
